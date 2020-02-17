5bf5ecbf20106086bf7e7ae8e7ca1d94ae9ce3b1
GeorgiArn <georgearnaudov1@gmail.com>
2020-02-17 21:00:22 +0200

    Create router for auth module

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,10 @@
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: 'auth'
+  }
+];
+
+export const AuthRoutingModule = RouterModule.forChild(routes);
