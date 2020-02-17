cc62c68fcd141326e24228ae0679589771037168
GeorgiArn <georgearnaudov1@gmail.com>
2020-02-17 12:37:40 +0200

    Create layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavigationComponent } from './navigation/navigation.component';
+
+
+
+@NgModule({
+  declarations: [NavigationComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [NavigationComponent]
+})
+export class LayoutModule { }
