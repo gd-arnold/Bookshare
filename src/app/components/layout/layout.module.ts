3cdfe03dcc1e3f910509d2a18270b14ab3b273ae
GeorgiArn <georgearnaudov1@gmail.com>
2020-02-18 22:13:47 +0200

    Update deployment

--- src/app/components/layout/layout.module.ts
+++ src/app/components/layout/layout.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { RouterModule } from '@angular/router';
 import { SearchEngineComponent } from '../shared/search-engine/search-engine.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [NavigationComponent, SearchEngineComponent],
+  declarations: [NavigationComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    SharedModule
   ],
   exports: [NavigationComponent]
 })
