98de53c1ac0980eb7404a7fc90430cdf488c312e
GeorgiArn <georgearnaudov1@gmail.com>
2020-02-21 23:58:39 +0200

    Check if current user has current book

--- src/app/components/book/book-detail/book-detail.component.ts
+++ src/app/components/book/book-detail/book-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BookService } from 'src/app/core/services/book.service';
 import { IBook } from '../../shared/interfaces/book';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-book-detail',
@@ -11,15 +12,22 @@ import { IBook } from '../../shared/interfaces/book';
 export class BookDetailComponent implements OnInit {
 
   get book() { return this.bookService.book; }
+  get currentUserData() { return this.authService.currentUser; }
 
   constructor(
     private router: ActivatedRoute,
-    private bookService: BookService
+    private bookService: BookService,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
     let bookId = this.router.snapshot.paramMap.get('id');
     this.bookService.fetchBookById(bookId);
+    if (this.authService.isAuth) { this.authService.getCurrentUserBasicData(); }
+  }
+
+  isOwner() {
+    return this.book.users.some(user => user["id"] === this.currentUserData.id)
   }
 
 }
