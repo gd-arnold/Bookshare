dd4f258053bed0fd48fd25404e8ce6a941504e00
HristoStoynov <46325638+HristoStoynov@users.noreply.github.com>
2020-05-21 15:34:27 +0300

    profile-update

--- src/app/components/user/user-profile/user-profile.component.ts
+++ src/app/components/user/user-profile/user-profile.component.ts
@@ -10,6 +10,36 @@ export class UserProfileComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let bars = document.querySelectorAll('.bar');
+    let arrayBars = Array.from(bars);
+
+    arrayBars.forEach(bar => {
+      bar.addEventListener('click', () => {
+        bar.className = "col-4 bar border-right h5"
+        let divsId = bar.textContent;
+
+        //document.querySelector(`#${divsId}`).style.display = 'none';
+
+        let div = document.querySelector(`#${divsId}`)
+        div.style.display = 'block';
+
+        let divMenus = document.querySelectorAll('.div-menu')
+        let arrayDivMenus = Array.from(divMenus);
+
+        arrayDivMenus.forEach(divMenu => {
+          if (divMenu !== div) {
+            divMenu.style.display = 'none';
+          }
+        });
+
+        arrayBars.forEach(otherBars => {
+          if (bar !== otherBars) {
+            otherBars.className = "col-4 bar border-right h5 border-bottom"
+          }
+        })
+
+      })
+    });
   }
 
 }
