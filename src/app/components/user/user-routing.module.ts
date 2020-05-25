0042607b52cd4654fefb8127c84330debe6e33f3
GeorgiArn <georgearnaudov1@gmail.com>
2020-05-25 12:12:46 +0300

    Set up admin panel

--- src/app/components/user/user-routing.module.ts
+++ src/app/components/user/user-routing.module.ts
@@ -1,6 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { AuthGuard } from 'src/app/core/guards/auth.guard';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 
 const routes: Routes = [
@@ -10,6 +12,11 @@ const routes: Routes = [
         {
             path: 'profile',
             component: UserProfileComponent,
+            canActivate: [AuthGuard]
+        },
+        {
+            path: 'admin',
+            component: AdminPanelComponent,
             canActivate: [AuthGuard]
         }
     ]
