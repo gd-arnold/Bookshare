e2656f5ef07ab59a44dd0b1d89f44d4b847a6b27
GeorgiArn <georgearnaudov1@gmail.com>
2020-05-29 23:15:01 +0300

    Set up support page

--- src/app/components/user/user-routing.module.ts
+++ src/app/components/user/user-routing.module.ts
@@ -1,6 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { AuthGuard } from 'src/app/core/guards/auth.guard';
+import { SupportComponent } from './support/support.component';
 
 
 const routes: Routes = [
@@ -10,6 +11,11 @@ const routes: Routes = [
         {
             path: 'profile',
             component: UserProfileComponent,
+            canActivate: [AuthGuard]
+        }, 
+        {
+            path: 'support',
+            component: SupportComponent,
             canActivate: [AuthGuard]
         }
     ]
