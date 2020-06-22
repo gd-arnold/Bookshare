6896894605c18e920d5cfe9dee98510b26bd73c3
GeorgiArn <georgearnaudov1@gmail.com>
2020-06-22 11:46:18 +0300

    Suggest book and send message only is user is auth

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/core/services/user.service';
 import { BookService } from 'src/app/core/services/book.service';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-footer',
@@ -10,8 +11,13 @@ import { BookService } from 'src/app/core/services/book.service';
 export class FooterComponent implements OnInit {
 
   constructor
-  (private userService: UserService,
-    private bookService: BookService) { }
+    (
+      private userService: UserService,
+      private bookService: BookService,
+      private authService: AuthService
+    ) { }
+
+  get isAuth() { return this.authService.isAuth; }
 
   ngOnInit() {
   }
